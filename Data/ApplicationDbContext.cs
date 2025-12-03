ade15775549e83d6b54ff0f9af9905ac5fc4f694
alfonso1410 <105889509+alfonso1410@users.noreply.github.com>
2025-12-02 20:29:43 -0600

    TALLER ASP_NET.CORE

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Taller_ASP.Models;
+
+namespace Taller_ASP.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Taller_ASP.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "El titulo es obligatorio")]
+        [StringLength(100, ErrorMessage = "El titulo no puede tener más de 100 caracteres")]
+        public string Title { get; set; }
+
+        [StringLength(100, ErrorMessage = "La descripcion no puede tener más de 100 caracteres")]
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+
+        public int Order { get; set; } = 0;
+
+
+        public byte[]? Image { get; set; }
+
+        public string? ImageContentType { get; set; }
+        [NotMapped]
+
+        public IFormFile? ImageFile { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        [BindNever]
+        public string UserId { get; set; } = string.Empty;
+
+
+    }
+}
